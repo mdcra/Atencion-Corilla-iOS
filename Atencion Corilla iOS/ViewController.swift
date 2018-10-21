b14472600115969bc7b5c5b804bbf6a31f68a0b6
mdcra <maria.ramos21@upr.edu>
2018-10-21 11:20:41 -0400

    Initial Commit

---
+++ Atencion Corilla iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Atencion Corilla iOS
+//
+//  Created by Maria del Carmen Ramos Alamo on 10/21/18.
+//  Copyright © 2018 Maria del Carmen Ramos Alamo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
